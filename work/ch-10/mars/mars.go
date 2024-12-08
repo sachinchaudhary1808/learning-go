a7a1600041a4975bbf873f1ce8cc576f1e5aead5
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-12-08 19:06:35 +0530

    updated

---
+++ work/ch-10/mars/mars.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 41.0
+	marsDays := 687.0
+	earthDays := 365.2425
+
+	fmt.Println("I am", age*earthDays/marsDays, "years old on Mars.")
+}
--- work/ch-10/typesdontmix/foo.go
+++ work/ch-10/typesdontmix/foo.go
@@ -6,5 +6,5 @@ func main() {
 	countdown := "Launch in T minus " + "10 seconds."
 	fmt.Println(countdown)
 
-	// countdown = "launch in T minus " + 10 + " seconds."
+	countdown = "launch in T minus " + 10 + " seconds."
 }
