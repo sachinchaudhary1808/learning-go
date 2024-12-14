ccf12f77498cc9367f2e026932489565136c1ea4
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-12-14 19:33:20 +0530

    first commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "python pratice";
+
+  inputs = {nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";};
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShells = {
+      x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShellNoCC {
+        buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [go_1_22 gopls revive];
+
+        shellHook = ''
+          echo "go code coco!"
+        '';
+      };
+    };
+  };
+}
---
+++ work/calculator/calculator.go
@@ -0,0 +1,20 @@
+// Package calculator does simple calculations.
+package calculator
+
+// Add takes Two numbers and returns the result of adding
+// them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+// Subtract takes two numbers a and b, and
+// returns the result of subtracting b from a.
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+// Multiply takes two number a and b, and
+// returns the result of multiplying  a and b.
+func Multiply(a, b float64) float64 {
+	return a * b
+}
