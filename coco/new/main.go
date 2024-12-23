d071ccc21e7a08fc86342390f7adf7cd6df08ea3
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-12-23 23:50:43 +0530

    first goal done of ch-4

--- coco/new/main.go
+++ coco/new/main.go
@@ -5,9 +5,12 @@ import "fmt"
 func main() {
 	var title string
 	var copies int
+	var author string
 	title = "For the Love of Go"
 	copies = 99
+	author = "John Arundel"
 	fmt.Println(title)
 	fmt.Println(copies)
+	fmt.Println(author)
 
 }
