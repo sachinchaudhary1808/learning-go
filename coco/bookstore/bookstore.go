5ac1d76172eebf0b6580845192f222354ce5871a
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-01-23 14:44:35 +0530

    did chapter 5

--- coco/bookstore/bookstore.go
+++ coco/bookstore/bookstore.go
@@ -1,9 +1,20 @@
 // Package bookstore pkg
 package bookstore
 
+import "errors"
+
 // Book represents information about a book.
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+}
+
+// Buy function
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	}
+	b.Copies--
+	return b, nil
 }
--- coco/bookstore/bookstore_test.go
+++ coco/bookstore/bookstore_test.go
@@ -14,3 +14,39 @@ func TestBook(t *testing.T) {
 	}
 
 }
+
+// TestBuy buy funtion
+func TestBuy(t *testing.T) {
+
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 2,
+	}
+
+	want := 1
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d, go %d", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Errorf("want error buying from zero copies, got nil")
+	}
+
+}
